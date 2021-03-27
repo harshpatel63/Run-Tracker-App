a7452834cf9718e98602f0662f0caad23fd14b39
Harsh <patelharsh0603@gmail.com>
2021-03-27 22:49:01 +0530

    Setting up map view in TrackingFragment.kt. Initializing tracking service to print log statements.

--- app/src/main/java/com/example/runtrackerapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/runtrackerapp/ui/fragments/TrackingFragment.kt
@@ -1,9 +1,19 @@
 package com.example.runtrackerapp.ui.fragments
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.runtrackerapp.R
+import com.example.runtrackerapp.databinding.FragmentRunBinding
+import com.example.runtrackerapp.databinding.FragmentTrackingBinding
+import com.example.runtrackerapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.example.runtrackerapp.services.TrackingService
 import com.example.runtrackerapp.ui.viewmodels.MainViewModel
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -11,4 +21,64 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private val viewModel: MainViewModel by viewModels()
 
+    private var map: GoogleMap? = null
+
+    private var _binding: FragmentTrackingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTrackingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+
+            mapView.onCreate(savedInstanceState)
+                btnToggleRun.setOnClickListener{
+                    sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+                }
+            mapView.getMapAsync {
+                map = it
+                }
+
+            }
+        }
+    private fun sendCommandToService(action: String){
+        Intent(requireContext(), TrackingService::class.java).also {
+            it.action = action
+            requireContext().startService(it)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
 }
