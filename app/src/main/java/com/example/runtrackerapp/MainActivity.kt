15355187f5324fffd5cc83bf752f6e6d298e57f9
Harsh <patelharsh0603@gmail.com>
2021-03-27 16:54:55 +0530

    Added binding to main activity

--- app/src/main/java/com/example/runtrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/runtrackerapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.runtrackerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.runtrackerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
